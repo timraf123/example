83454e9a08ea1528e402fe8b5d5103eca8f4ebe1
timraf123 <74038553+timraf123@users.noreply.github.com>
2020-11-12 17:48:12 +1100

    Add files via upload
    monte-carlo

--- fin2.py
+++ fin2.py
@@ -11,6 +11,9 @@ import json
 import demjson
 import sys
 from pandas_datareader import data as pdr
+import numpy as np
+import seaborn as sns
+
 fyf.pdr_override() 
 from pyblackscholesanalytics.market.market import MarketEnvironment
 from pyblackscholesanalytics.portfolio.portfolio import Portfolio
@@ -100,8 +103,74 @@ def setPortfolio():
     option_spread_ptf.add_instrument(vcl, 1)
     option_spread_ptf.add_instrument(vcs, -1)
     print(option_spread_ptf)
-
     
+def calc_commission_rate(x):
+    """ Return the commission rate based on the table:
+    0-90% = 2%
+    91-99% = 3%
+    >= 100 = 4%
+    """
+    if x <= .90:
+        return .02
+    if x <= .99:
+        return .03
+    else:
+        return .04
+
+def monteCarlo():
+    print("*** Monte-Carlo ***")  
+    sns.set_style('whitegrid')
+    avg = 1
+    std_dev = .1
+    num_reps = 500
+    num_simulations = 1000
+
+    pct_to_target = np.random.normal(avg, std_dev, num_reps).round(2)
+    print(pct_to_target)
+    
+    sales_target_values = [75_000, 100_000, 200_000, 300_000, 400_000, 500_000]
+    sales_target_prob = [.3, .3, .2, .1, .05, .05]
+    sales_target = np.random.choice(sales_target_values, num_reps, p=sales_target_prob)
+    print(sales_target)
+    
+    df = pd.DataFrame(index=range(num_reps), data={'Pct_To_Target': pct_to_target,
+                                               'Sales_Target': sales_target})
+
+    df['Sales'] = df['Pct_To_Target'] * df['Sales_Target']
+    print(df)
+    df['Commission_Rate'] = df['Pct_To_Target'].apply(calc_commission_rate)
+    df['Commission_Amount'] = df['Commission_Rate'] * df['Sales']
+    print(df)
+    
+    all_stats = []
+
+# Loop through many simulations
+    for i in range(num_simulations):
+
+        # Choose random inputs for the sales targets and percent to target
+        sales_target = np.random.choice(sales_target_values, num_reps, p=sales_target_prob)
+        pct_to_target = np.random.normal(avg, std_dev, num_reps).round(2)
+    
+        # Build the dataframe based on the inputs and number of reps
+        df = pd.DataFrame(index=range(num_reps), data={'Pct_To_Target': pct_to_target,
+                                                       'Sales_Target': sales_target})
+    
+        # Back into the sales number using the percent to target rate
+        df['Sales'] = df['Pct_To_Target'] * df['Sales_Target']
+    
+        # Determine the commissions rate and calculate it
+        df['Commission_Rate'] = df['Pct_To_Target'].apply(calc_commission_rate)
+        df['Commission_Amount'] = df['Commission_Rate'] * df['Sales']
+    
+        # We want to track sales,commission amounts and sales targets over all the simulations
+        all_stats.append([df['Sales'].sum().round(0),
+                          df['Commission_Amount'].sum().round(0),
+                          df['Sales_Target'].sum().round(0)])
+        
+    results_df = pd.DataFrame.from_records(all_stats, columns=['Sales',
+                                                           'Commission_Amount',
+                                                           'Sales_Target'])
+    results_df.describe().style.format('{:,}')
 def main():
     print("*** Stock Price ***")
     getStockPrice()
@@ -112,3 +181,4 @@ if __name__ == "__main__":
     print(msft.info)
     test_matplot()
     setPortfolio()
+    monteCarlo()
